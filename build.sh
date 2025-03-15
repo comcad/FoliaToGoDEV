5aa7667549913287c6848b97f78d05f178b745b9
comcad <briannobre@hotmail.com>
2025-03-15 11:10:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/PaperMC/Folia/tree/dev/1.21.4
+git clone -b dev/1.21.4 https://github.com/PaperMC/Folia.git
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
